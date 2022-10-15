6169d697b2254226e65e8ac597544600a0ace1ae
CoelhoTheDourado <110208982+CoelhoTheDourado@users.noreply.github.com>
2022-10-15 20:35:15 -0300

    Add files via upload

---
+++ assets/preload/custom_notetypes/staticNote.lua
@@ -0,0 +1,14 @@
+function onCreate()
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'staticNote' then 
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'staticNote');
+
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') then --Doesn't let Dad/Opponent notes get ignored
+				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has penalties
+			end
+		end
+	end
+end
+
+
+
